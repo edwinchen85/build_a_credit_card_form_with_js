5868519cd185698a56fc1e55261cc1a7127abaeb
Edwin Chen <edwinchen85@gmail.com>
2017-06-16 21:55:58 +0800

    Add input mask to cvv field

--- js/scripts.js
+++ js/scripts.js
@@ -11,5 +11,6 @@ $( function() {
   // Set the masks.
   number.inputmask( "9999 9999 9999 9[999] [999]", { "placeholder": " " } );
   expDate.inputmask( "mm/yyyy" );
+  cvv.inputmask( "999[9]", { "placeholder": " " } );
 
 });
